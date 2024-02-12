26bcc6a03a7ef011319ab7254a00debb2f1078e2
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-02-12 09:09:44 -0300

    feat(component): method utility to get names of the properties null

---
+++ src/main/java/br/com/zendteam/hr/infra/CopyNonNull.java
@@ -0,0 +1,31 @@
+package br.com.zendteam.hr.infra;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class CopyNonNull {
+    protected void copyNonNullProperties(Object source, Object target) {
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+
+    // Método utilitário para obter nomes de propriedades nulas
+    protected String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+}
